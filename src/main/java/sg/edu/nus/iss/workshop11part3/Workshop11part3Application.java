4e2dfe3cff5085d3a8ccfd53e9b6f7564ccda862
Alejandro-houlu <alejandro.hlcw@gmail.com>
2022-02-28 16:12:32 +0800

    deploy to heroku

---
+++ src/main/java/sg/edu/nus/iss/workshop11part3/Workshop11part3Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.workshop11part3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop11part3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshop11part3Application.class, args);
+	}
+
+}
